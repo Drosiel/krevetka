3674db314b7b66afccd05a18371ede5ed79325f5
Grimm <repisk@gmail.com>
2020-09-22 13:47:49 +0500

    krevetka

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './js/main';
+import sprite from "./img/sprite.svg";
+import img from "./img/img1.png";
+import logo from "./img/logo.png"
+import './sass/style.sass';
+
+import React from "react";
+import { render } from "react-dom";
+
+//import App from "./js/App";
+
+//render( <App />, document.getElementById("app"));
